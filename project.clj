ca1cdb9414ff4a7e9c790327a6c04e7d87209874
Harold <harold@thinktopic.com>
2016-04-05 15:52:05 -0600

    [project] Updating dependencies.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject mario-net "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.mikera/vectorz-clj "0.44.0"]
+                 [net.mikera/core.matrix "0.51.0"]
                  [thinktopic/cortex "0.1.0-SNAPSHOT"]
-                 [com.taoensso/nippy "2.9.0"]
+                 [com.taoensso/nippy "2.11.1"]
                  [net.mikera/imagez "0.10.0"]])
